ec6e9c61cd43f66cc6a00ad0c8ef8dcd69888de9
Nabil Khalid <holanabilkhalid@gmail.com>
2023-01-18 18:34:35 +0800

    Add import files from page.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import _ from 'lodash';
-import pageLoad from './page';
 import './style.css';
+import { pageLoad, tabContent } from './page';
 
-// function createBtn(name) {
-//     const btn = document.createElement('button');
-//     btn.textContent = name;
-//     document.querySelector('#content').appendChild(btn);
-// }
-
-// createBtn('Home');
-// createBtn('Menu');
-// createBtn('Contact');
-
-pageLoad.setHeader();
-pageLoad.createBtn('Home');
-pageLoad.createBtn('Menu');
-pageLoad.createBtn('Contact');
-pageLoad.buttonEvent();
+pageLoad.setPage();
+tabContent.home();
+tabContent.buttonEvent();
