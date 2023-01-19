b74642b97d2c147ddb6d856a00759ce9c4ba86c9
Nabil Khalid <holanabilkhalid@gmail.com>
2023-01-19 19:28:13 +0800

    Move buttonEvent from page.js into index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import _ from 'lodash';
 import './style.css';
-import { pageLoad, tabContent } from './page';
+import pageLoad from './page';
+import home from './home';
+import menu from './menu';
+import contact from './contact';
 
 pageLoad.setPage();
-tabContent.home();
-tabContent.buttonEvent();
+home.renderHome();
+
+const buttonEvent = () => {
+    const redirectMenuBtn = document.querySelector('.to-menu');
+    const button = document.querySelectorAll('header button');
+    redirectMenuBtn.addEventListener('click', () => menu.renderMenu());
+    button.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            switch (e.target.className) {
+                case 'home':
+                    home.renderHome();
+                    break;
+                case 'menu':
+                    menu.renderMenu();
+                    break;
+                case 'contact':
+                    contact.renderContact();
+                    break;
+            }
+        });
+    });
+};
+
+buttonEvent();
